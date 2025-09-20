102a722d74c5a1e76f5e7992180d44c51ed0287c
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-09-20 14:31:43 +0200

    chore: Edit the example.com html

--- example.user.js
+++ example.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name            Example Tampermonkey Script
 // @description     Example
-// @version         1.0
+// @version         1.0.0
 // @author          TonimatasDEV
 // @homepageURL     https://github.com/TonimatasDEV/example-tampermonkey-script
 // @supportURL      https://github.com/TonimatasDEV/example-tampermonkey-script/issues
 // @match           https://*.example.com/*
 // @license         MIT
 // @namespace       https://github.com/TonimatasDEV/example-tampermonkey-script
-// @icon            none
+// @icon            https://cdn-icons-png.flaticon.com/512/5129/5129538.png
 // @downloadURL     https://raw.githubusercontent.com/TonimatasDEV/example-tampermonkey-script/master/example.user.js
 // @contributionURL https://ko-fi.com/tonimatasdev
 // @grant           none
@@ -17,6 +17,9 @@
 
 (function () {
     'use strict';
+
+    const primaryContent = document.querySelector("div");
+    primaryContent.innerHTML = primaryContent.innerHTML + "<p style='color: green'>Your example script is here and working correctly!<p/>"
 
     console.log('¡Hello from your script!');
 })();
