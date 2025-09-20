cc825071d223b4f66ef3514cdf32a441793183a6
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-09-20 14:18:51 +0200

    Initial commit

---
+++ example.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name            Example Tampermonkey Script
+// @description     Example
+// @version         1.0
+// @author          TonimatasDEV
+// @homepageURL     https://github.com/TonimatasDEV/example-tampermonkey-script
+// @supportURL      https://github.com/TonimatasDEV/example-tampermonkey-script/issues
+// @match           https://*.example.com/*
+// @license         MIT
+// @namespace       https://github.com/TonimatasDEV/example-tampermonkey-script
+// @icon            none
+// @downloadURL     https://raw.githubusercontent.com/TonimatasDEV/example-tampermonkey-script/master/example.user.js
+// @contributionURL https://ko-fi.com/tonimatasdev
+// @grant           none
+// @attribution     Original project (https://github.com/TonimatasDEV/example-tampermonkey-script)
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('¡Hello from your script!');
+})();
